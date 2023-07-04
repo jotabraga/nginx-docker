83a54cf2481d4355da6a5e5bd65e7a9ef5522940
jotabraga <jpmbraga.feg@gmail.com>
2023-07-04 15:36:25 -0300

    feat: add init db sql file to automatic mysql tables setup

--- node/index.js
+++ node/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
 const port = 3000;
+
+const connection = mysql.createConnection({
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+});
 
 app.get("/", (_req, res) => {
   res.send("Teste");
